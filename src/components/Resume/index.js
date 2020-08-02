a632ad0896836eb4c4440c0e43089c60f7065be2
Melissa Daskalantonakis <melissa.daska@gmail.com>
2020-08-02 09:55:19 -0500

    update

---
+++ src/components/Resume/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Resume() {
+    return(
+        <section className="my-5">
+            <h1 id="about">Resume</h1>
+            <p>View my LinkedIn to see resume <a href="https://www.linkedin.com/in/melissa-daskalantonakis/">Melissa's LinkedIn</a>.</p>
+
+            <h3>Technical Skills</h3>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>React</li>
+                    <li>Node</li>
+                    <li>Express</li>
+                    <li>MySQL, Sequelize</li>
+                    <li>MongoDB, Mongoose</li>
+                </ul>
+        </section>
+    )
+}
+
+export default Resume;
