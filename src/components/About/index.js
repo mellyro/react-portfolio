267d6e6c824c2f5a57450a7b44607b406a4b41a6
Melissa Daskalantonakis <melissa.daska@gmail.com>
2020-08-07 17:07:12 -0500

    move skills to resume component

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,17 +1,6 @@
 import React from 'react';
 import coverImage from '../../assets/profile-picture/profilepic.png';
 import Jumbotron from '../Jumbotron';
-import html from '../../assets/skills/html.png';
-import bootstrap from '../../assets/skills/bootstrap.png';
-import css from '../../assets/skills/css.png';
-import github from '../../assets/skills/github.png';
-import handlebars from '../../assets/skills/handlebars.png';
-import javascript from '../../assets/skills/javascript.png';
-import mern from '../../assets/skills/mern.png';
-import mongodb from '../../assets/skills/mongodb.png';
-import mysql from '../../assets/skills/mysql.png';
-import node from '../../assets/skills/nodejs.png';
-import react from '../../assets/skills/react.png';
 import '../../index.css';
 
 
@@ -28,35 +17,6 @@ const About = () => (
                 <h4 className="bio bio1">
                 My name is Melissa Daskalantonakis. I reside in Austin, TX with my husband and two loving dogs. I graduated from The University of Texas at Austin in 2014 and I have been an Assistant Property Manager for office buildings for about 6 years now. I have decided to explore the world of web development. I find React very intriguing!
                 </h4>
-            </div>
-        </div>
-        <h3 className="skills">My Skills</h3>
-        <hr />
-        <div className="row">
-            <div className="col">
-                <div className="row">
-                    <div className="col-sm text-center">
-                        <img src={html} style={{ width: "10%" }} alt="html" />
-                        <img src={bootstrap} style={{ width: "10%" }} alt="bootstrap" />
-                        <img src={css} style={{ width: "10%" }} alt="css" />
-                        <img src={github} style={{ width: "10%" }} alt="github" />
-                        <img src={handlebars} style={{ width: "10%" }} alt="handlebars" />
-                        <img src={javascript} style={{ width: "10%" }} alt="javascript" />
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div className="row">
-            <div className="col">
-                <div className="row">
-                    <div className="col-sm text-center">
-                        <img src={mern} style={{ width: "10%" }} alt="mern" />
-                        <img src={mongodb} style={{ width: "10%" }} alt="mongodb" />
-                        <img src={mysql} style={{ width: "10%" }} alt="mysql" />
-                        <img src={node} style={{ width: "10%" }} alt="node" />
-                        <img src={react} style={{ width: "10%" }} alt="react" />
-                    </div>
-                </div>
             </div>
         </div>
     </div>
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,25 +1,61 @@
 import React from 'react';
+import html from '../../assets/skills/html.png';
+import bootstrap from '../../assets/skills/bootstrap.png';
+import css from '../../assets/skills/css.png';
+import github from '../../assets/skills/github.png';
+import handlebars from '../../assets/skills/handlebars.png';
+import javascript from '../../assets/skills/javascript.png';
+import mern from '../../assets/skills/mern.png';
+import mongodb from '../../assets/skills/mongodb.png';
+import mysql from '../../assets/skills/mysql.png';
+import node from '../../assets/skills/nodejs.png';
+import react from '../../assets/skills/react.png';
 import '../../index.css';
 
 function Resume() {
     return(
-        <section className="my-5">
-            <h1 id="about">Resume</h1>
-            <p>View my LinkedIn to see resume <a href="https://www.linkedin.com/in/melissa-daskalantonakis/">Melissa's LinkedIn</a>.</p>
+        <section>
+            <div>
+            <h3 className="col-sm text-center">Resume</h3>
+                <div className="col-sm text-center">
+                    <p>View my LinkedIn to see resume <a href="https://www.linkedin.com/in/melissa-daskalantonakis/">Melissa's LinkedIn</a>.</p>
+                </div>
+            </div>
 
-            <h3>Technical Skills</h3>
-                <ul>
-                    <li>HTML</li>
-                    <li>CSS</li>
-                    <li>JavaScript</li>
-                    <li>React</li>
-                    <li>Node</li>
-                    <li>Express</li>
-                    <li>MySQL, Sequelize</li>
-                    <li>MongoDB, Mongoose</li>
-                </ul>
+            <br />
+            <br />
+
+            <h3 className="col-sm text-center">Technical Skills</h3>
+            <br />
+            <div className="row">
+                <div className="col">
+                    <div className="row">
+                        <div className="col-sm text-center">
+                            <img src={html} style={{ width: "10%" }} alt="html" />
+                            <img src={bootstrap} style={{ width: "10%" }} alt="bootstrap" />
+                            <img src={css} style={{ width: "10%" }} alt="css" />
+                            <img src={github} style={{ width: "10%" }} alt="github" />
+                            <img src={handlebars} style={{ width: "10%" }} alt="handlebars" />
+                            <img src={javascript} style={{ width: "10%" }} alt="javascript" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <div className="row">
+                        <div className="col-sm text-center">
+                            <img src={mern} style={{ width: "10%" }} alt="mern" />
+                            <img src={mongodb} style={{ width: "10%" }} alt="mongodb" />
+                            <img src={mysql} style={{ width: "10%" }} alt="mysql" />
+                            <img src={node} style={{ width: "10%" }} alt="node" />
+                            <img src={react} style={{ width: "10%" }} alt="react" />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
-    )
+    );
 }
 
 export default Resume;
