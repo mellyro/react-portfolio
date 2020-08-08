c5468f369db73042a208d8ab5ef962d6730f7e67
Melissa Daskalantonakis <melissa.daska@gmail.com>
2020-08-08 13:56:43 -0500

    Updates

---
+++ precache-manifest.47c9e99241060b11c1430431525f0464.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95391ec4e61b48ac5b102e8d6908ae7e",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "642953d01bda8d0f3dc1",
+    "url": "/react-portfolio/static/css/main.819d1fe0.chunk.css"
+  },
+  {
+    "revision": "de12b6cf26bf7e8320f0",
+    "url": "/react-portfolio/static/js/2.ba1889b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.ba1889b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "642953d01bda8d0f3dc1",
+    "url": "/react-portfolio/static/js/main.fe956320.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "eb258788826be3da8565761a3512908d",
+    "url": "/react-portfolio/static/media/bootstrap.eb258788.png"
+  },
+  {
+    "revision": "3281b3d275ce31b8c658e3f287ab1629",
+    "url": "/react-portfolio/static/media/foodfestival.3281b3d2.png"
+  },
+  {
+    "revision": "1150ff21be3b817f26bb89810f7e08aa",
+    "url": "/react-portfolio/static/media/handlebars.1150ff21.png"
+  },
+  {
+    "revision": "f12a3dbf8da5da67f7698f8519939d46",
+    "url": "/react-portfolio/static/media/hearth.f12a3dbf.png"
+  },
+  {
+    "revision": "43636350ac83f788f00823356726c0e3",
+    "url": "/react-portfolio/static/media/horiseon.43636350.png"
+  },
+  {
+    "revision": "6a4c201cef84c174d0d93923f26b168c",
+    "url": "/react-portfolio/static/media/javascript.6a4c201c.png"
+  },
+  {
+    "revision": "121cbb28ae2b72c95f663e1e9e802d9e",
+    "url": "/react-portfolio/static/media/mern.121cbb28.png"
+  },
+  {
+    "revision": "daeaaf51189d49c32411056c83360f41",
+    "url": "/react-portfolio/static/media/mongodb.daeaaf51.png"
+  },
+  {
+    "revision": "efd22267efa1c9589fe5611454dfe821",
+    "url": "/react-portfolio/static/media/mysql.efd22267.png"
+  },
+  {
+    "revision": "f7337d339216d05c1551688efb13a830",
+    "url": "/react-portfolio/static/media/nodejs.f7337d33.png"
+  },
+  {
+    "revision": "74d5e80db4ca144a5b52d09abb50b92e",
+    "url": "/react-portfolio/static/media/paperlocator.74d5e80d.png"
+  },
+  {
+    "revision": "fd286bb2155ec2ae0cfbcce20d53f3e5",
+    "url": "/react-portfolio/static/media/photoport.fd286bb2.png"
+  },
+  {
+    "revision": "e86e8b13add426a2b469a952bc3e4b4e",
+    "url": "/react-portfolio/static/media/profilepic.e86e8b13.png"
+  },
+  {
+    "revision": "6e8f68df1509ab98bffab08b6f001c56",
+    "url": "/react-portfolio/static/media/react.6e8f68df.png"
+  },
+  {
+    "revision": "3c6f93210c25c7ab3a07ca1094ee3469",
+    "url": "/react-portfolio/static/media/runbuddy.3c6f9321.png"
+  }
+]);
