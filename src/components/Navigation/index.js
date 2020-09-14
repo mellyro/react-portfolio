fd203c8384f4a673b0074329bdbc3ae712b12b51
Melissa Daskalantonakis <melissa.daska@gmail.com>
2020-09-13 19:33:02 -0500

    try to fix nav again

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -7,21 +7,21 @@ function Navigation() {
         <Nav variant="pills" defaultActiveKey="/melissa-daska/#/about" className="row navigation" id="navigation">
             
             <Nav.Item>
-                <Nav.Link eventKey="/home" href="/melissa-daska/#/home" id="hometab">
+                <Nav.Link eventKey="/home" href="/react-portfolio/#/home" id="hometab">
                     <h1 className="logo">M<span>D</span></h1>
                 </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link href="/melissa-daska/#/about" className="" >About</Nav.Link>
+                <Nav.Link href="/react-portfolio/#/about" className="" >About</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link eventKey="/portfolio" href="/melissa-daska/#/portfolio">Portfolio</Nav.Link>
+                <Nav.Link eventKey="/portfolio" href="/react-portfolio/#/portfolio">Portfolio</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link eventKey="/contact" href="/melissa-daska/#/contact">Contact</Nav.Link>
+                <Nav.Link eventKey="/contact" href="/react-portfolio/#/contact">Contact</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link eventKey="/resume" href="/melissa-daska/#/resume">Resume</Nav.Link>
+                <Nav.Link eventKey="/resume" href="/react-portfolio/#/resume">Resume</Nav.Link>
             </Nav.Item>
         </Nav>     
 
